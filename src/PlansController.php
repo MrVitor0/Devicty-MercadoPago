b468e169574355daf0f3d9ee252a0dfee27de9a0
Mrvitor0 <vto.hugo67@gmail.com>
2022-09-12 21:02:54 +0000

    Feature: Package Setup

---
+++ src/PlansController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Devicty\MercadoPago;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PlansController extends Controller
+{
+    public function index()
+    {
+       
+    }
+}
